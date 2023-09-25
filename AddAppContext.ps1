c164b3ffd2fac326d4b23a02e5a561af992b9919
SinghAmreek <110961553+SinghAmreek@users.noreply.github.com>
2023-09-25 16:59:01 +1000

    Scripts to get non-compliant flows
    Scripts to get non-compliant flows

---
+++ AddAppContext.ps1
@@ -0,0 +1,3 @@
+﻿Add-AdminFlowPowerAppContext -EnvironmentName cfd05356-f25c-4d53-a36f-eeabd47a63a2 -FlowName 8f873703-d36a-47c2-a4cf-8088bbf3091d -AppName ft_FRP
+
+remove-AdminFlowPowerAppContext -EnvironmentName cfd05356-f25c-4d53-a36f-eeabd47a63a2 -FlowName 8f873703-d36a-47c2-a4cf-8088bbf3091d -AppName ft_FRP
---
+++ PA suspension list.ps1
@@ -0,0 +1,25 @@
+﻿# Following modules are required to run suspension list scripts
+Install-Module -Name Microsoft.PowerApps.Administration.PowerShell -Force
+Install-Module -Name Microsoft.PowerApps.PowerShell -AllowClobber -Force 
+
+# Display the suspension list for a specific environment in the console window
+Get-AdminFlowAtRiskOfSuspension -EnvironmentName cfd05356-f25c-4d53-a36f-eeabd47a63a2 -ApiVersion '2016-11-01'
+
+# Add the suspension list for a specific environment in the console window
+Get-AdminFlowAtRiskOfSuspension -EnvironmentName cfd05356-f25c-4d53-a36f-eeabd47a63a2 -ApiVersion '2016-11-01' | Export-Csv -Path suspensionList.csv -NoTypeInformation
+
+Add-PowerAppsAccount
+$environments = Get-PowerAppEnvironment
+ForEach($environment in $environments){
+    $flowlist=Get-AdminFlowAtRiskOfSuspension -EnvironmentName $environment.EnvironmentName  -ApiVersion '2016-11-01' ##| Export-Csv -Path suspensionList.csv -NoTypeInformation
+    
+    if ( $flowlist.Length -gt 0)
+    {
+        
+        $filename= $environment.DisplayName + '.csv'
+        $flowlist| Export-Csv -Path $filename -NoTypeInformation
+    }
+
+#write-host $flowlist
+}
+
