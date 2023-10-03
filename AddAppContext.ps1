8891d9adf28226aa4e14a34a96d2ee2bf43debd2
SinghAmreek <110961553+SinghAmreek@users.noreply.github.com>
2023-10-04 10:42:28 +1100

    Final Files as shared in the demo video

--- AddAppContext.ps1
+++ AddAppContext.ps1
@@ -1,3 +1,14 @@
-﻿Add-AdminFlowPowerAppContext -EnvironmentName cfd05356-dddd-4d53-a36f-eeabd47a63a2 -FlowName 8f873703-d36a-4799-a4cf-8088bbf3091d -AppName ft_FRP
+﻿## Associate flow with an app - Change the parrameters to match your EnvironmentName, FlowName and AppName. 
+Add-AdminFlowPowerAppContext -EnvironmentName fc56583f-c243-6565-95bb-2f2d8437b3ca -FlowName 1063c97f-44f8-6272-893c-91b0cec27e9b -AppName ft_FRP
 
-remove-AdminFlowPowerAppContext -EnvironmentName cfd05356-dddd-4d53-a36f-eeabd47a63a2 -FlowName 8f873703-d36a-4799-a4cf-8088bbf3091d -AppName ft_FRP
+
+## This scripts read the EnvironmentName, FlowName and AppName from the file and run the  Add-AdminFlowPowerAppContex for every row in the csv file.
+$contextflows = Import-Csv -Path suspensionlist1.csv
+ForEach($contextflow in $contextflows)
+{
+    Add-AdminFlowPowerAppContext -EnvironmentName $contextflow.EnvironmentName -FlowName $contextflow.FlowName -AppName $contextflow.AppName
+}
+
+
+##  Remove the Associate of flow with an app - Change the parrameters to match your EnvironmentName, FlowName and AppName. 
+remove-AdminFlowPowerAppContext -EnvironmentName cfd05356-dddd-4d53-a36f-eeabd47a63a2 -FlowName 8f873703-d369-47c2-a4cf-8088bbf3091d -AppName ft_FRP
--- PA suspension list.ps1
+++ PA suspension list.ps1
@@ -2,11 +2,31 @@
 Install-Module -Name Microsoft.PowerApps.Administration.PowerShell -Force
 Install-Module -Name Microsoft.PowerApps.PowerShell -AllowClobber -Force 
 
+
+
 # Display the suspension list for a specific environment in the console window
 Get-AdminFlowAtRiskOfSuspension -EnvironmentName cfd05356-dddd-4d53-a36f-eeabd47a63a2 -ApiVersion '2016-11-01'
 
+
+
 # Add the suspension list for a specific environment in the console window
 Get-AdminFlowAtRiskOfSuspension -EnvironmentName cfd05356-dddd-4d53-a36f-eeabd47a63a2 -ApiVersion '2016-11-01' | Export-Csv -Path suspensionList.csv -NoTypeInformation
+
+
+# get the suspension list for a tenant. It works with first 500 orgs in the tenant
+$environments = Get-AdminPowerAppEnvironment
+
+$allFlows = @()
+foreach ($env in $environments) {
+    Write-Host "Getting flows at risk of suspension for environment $($env.DisplayName)..."
+    $flows = Get-AdminFlowAtRiskOfSuspension -EnvironmentName $env.EnvironmentName
+    Write-Host "Found $($flows.Count) flows at risk of suspension."
+    $allFlows += $flows
+}
+
+
+
+## get seperate file every org in the tenant
 
 Add-PowerAppsAccount
 $environments = Get-PowerAppEnvironment
@@ -19,7 +39,6 @@ ForEach($environment in $environments){
         $filename= $environment.DisplayName + '.csv'
         $flowlist| Export-Csv -Path $filename -NoTypeInformation
     }
-
-#write-host $flowlist
 }
+
 
